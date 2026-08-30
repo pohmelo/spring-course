5c080d02f4758719afa9c5934fc95c98c49b7f8e
pohmelo <mikkovp@hotmail.com>
2026-08-30 11:26:55 +0300

    Add Hello World controller

---
+++ src/main/java/fi/pohmelo/springcourse/rest/FunRestController.java
@@ -0,0 +1,15 @@
+package fi.pohmelo.springcourse.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that return "Hello World!"
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
